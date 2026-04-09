fb6b9e9c301c2c7ff15047ce5f2ce8182c29a3de
gustavonogvi <gustavonoggvinhas@gmail.com>
2026-04-09 17:32:32 -0300

    chore: adicionar CNPQ_URL

--- shared/urls.py
+++ shared/urls.py
@@ -1,1 +1,2 @@
 FUNCAP_URL = "http://montenegro.funcap.ce.gov.br/sugba/editais/"
+CNPQ_URL = "http://memoria2.cnpq.br/web/guest/chamadas-publicas?p_p_id=resultadosportlet_WAR_resultadoscnpqportlet_INSTANCE_0ZaM&filtro=abertas/"
